40fc56f69b0da9d2a8d1e7c2b454cabf67e40125
thucuyen04 <58126978+thucuyen04@users.noreply.github.com>
2019-12-05 11:53:33 -0500

    Update yelpProj.py

--- yelpProj.py
+++ yelpProj.py
@@ -3,7 +3,7 @@
 
 import pandas as pd
 import requests
-import matplotlib.pyplot as plt
+import tkinter
 
 userID = "2B41kI-lDbSxtgcBq-lSSw"
 
@@ -32,11 +32,30 @@ rest = a['businesses']
 if order == "rating":
     print("*Note that the rating sort is not strictly sorted by the rating value, but by an adjusted rating value that takes into account the number of ratings")
 
-for i in rest:
-    print("Name: " + i['name'])
-    print("Cuisine: " + i['categories'][0]['title'])
-    print(i['rating'])
-    print(i['location']['display_address'])
-    print("ID: " + i['id'])
-    print("__________")
+root = tkinter.Tk()
+root.title("Food")
+root.geometry("1000x1200")
+
+label = tkinter.Label(root, text = "Enter a city, state:")
+label.grid(row=0, sticky = "W")
+userInput = tkinter.Entry(root, width = 50)
+userInput.grid(row=0, column = 1)
+
+label1 = tkinter.Label(root, text = "Enter what you want to find:")
+label1.grid(row = 1)
+userInput1 = tkinter.Entry(root, text="Restaurants, cuisines, food", width = 50)
+userInput1.grid(row = 1, column = 1)
+
+def test(a):
+    for i in range(len(a)):
+        print(a[i]['id'])
+        return a[i]['id']
+
+def lst(a):
+    for i in range(len(a)):
+        b = tkinter.Button(root, height=5, width=50, text = 'Restaurant: ' + a[i]['name'] + '\n Category: ' + a[i]['categories'][0]['title'] + '\n Rating: ' + str(a[i]['rating']) + '\n Address: ' + a[i]['location']['display_address'][0] + ", " + a[i]['location']['display_address'][1], command = test(rest))
+        b.grid(row = i+2, column = 1)
+
+lst(rest)
+root.mainloop()
 
